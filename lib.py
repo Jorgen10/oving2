22288a355cb5a5b04600267a9741f4df60a05355
Jorgen_10 <strabjorgen@gmail.com>
2024-09-05 22:29:20 +0200

    preparing oving4

---
+++ lib.py
@@ -0,0 +1,8 @@
+def parseFloat(message: str) -> float:
+    answer = input(message)
+    try:
+        return float(answer)
+    except ValueError:
+        print(f"{answer} er ikke et godkjent tall. Prøv igjen!")
+        return parseFloat(message)
+    
---
+++ oppgave1.py
@@ -0,0 +1,8 @@
+from lib import parseFloat
+
+hoyde = parseFloat("Høyde (cm): ")
+
+if (hoyde >= 120):
+    print("Du kan kjøre karusellen!")
+else:
+    print("Du kan ikke kjøre karusellen.")
